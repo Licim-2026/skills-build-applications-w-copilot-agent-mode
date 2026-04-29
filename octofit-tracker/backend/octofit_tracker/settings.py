f01af3cbf681642084e9eef414ebf6cebad32a5c
Licim-2026 <lcimmermans@gmail.com>
2026-04-29 11:37:53 +0000

    Update Django settings and urls for Codespace public URL and API endpoint format

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -28,8 +28,12 @@ SECRET_KEY = 'django-insecure-nih0&y$qml@4@7-lg*h$q^^ct3m!&7ghz1#+5xo+y1v#2%0mri
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace public URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
